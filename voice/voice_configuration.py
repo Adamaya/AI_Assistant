d18dd7db0e7ad3b123ee1a079cf96f107b7d0017
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-04-09 02:02:19 +0530

    Voice_Configuration class and his methods are created in voice_configuration.py

---
+++ voice/voice_configuration.py
@@ -0,0 +1,26 @@
+import pyttsx3
+
+"""
+pyttsx3 is pytqhon library which is used to access the windows default voices 
+"""
+engine = pyttsx3.init('sapi5')
+
+
+class Voice_Configuration:
+    def configure_voice(self, voiceID):
+        """
+        This method configures the voice of the ai according to user requirements.
+        :param voiceID: it contains the voice id of selected voice by user.
+        """
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[voiceID].id)
+        engine.setProperty('rate', 120)
+        engine.setProperty('volume', 0.7)
+
+    def speak(self, speach):
+        """
+        this function returns the audio output of AI
+        :param speach: this parameter contains the voice command of the user in text format.
+        """
+        engine.say(speach)
+        engine.runAndWait()
