c5d9aadb75d963174f01e7f22026597c0b9037e6
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-11-04 23:45:09 +0530

    launch_ec2_instance() method created

---
+++ Features/awscli.py
@@ -0,0 +1,28 @@
+import subprocess, os
+
+
+def aws_authentication(access_key, secret_key, region):
+    access_key = "AKIAVTZV5EKIXFB6WG4Y"
+    secret_key = "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"
+    region = "ap-south-1"
+
+    os.system("aws configure set aws_access_key_id " + access_key)
+    os.system("aws configure set aws_secret_access_key " + secret_key)
+    os.system("aws configure set default.region " + region)
+    return None
+
+
+def launch_ec2_instance():
+    output = subprocess.getstatusoutput(
+        "aws ec2 run-instances --image-id ami-0e306788ff2473ccb --key-name myclikey --security-groups cliInstanceSecurityGroup --instance-type t2.micro")
+    return None
+
+
+def create_security_group():
+    output = subprocess.getstatusoutput(
+        'aws ec2 create-security-group --group-name "cliInstanceSecurityGroup" --description "this is the security group created for the new instance" --vpc-id vpc-3fe8f557')
+    print(output)
+    return None
+
+
+create_security_group()
