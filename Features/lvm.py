2cad12f9ddf6bf405bf5dd11ffcbe828915fe8f9
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-11-06 14:37:13 +0530

    create_physical_volume function create in lvm.py

--- Features/lvm.py
+++ Features/lvm.py
@@ -4,8 +4,19 @@ import subprocess as sp
 def attached_drive_report():
     """
     this function runs the fdisk -l command to show the attached drives to os
-    :return: attached drive report
+    :return: attached drive report with exit code
     """
     cmd = "sudo fdisk -l"
     output = sp.getstatusoutput(cmd)
-    return output[0]
+    return output
+
+
+def create_physical_volume(drive):
+    """
+    this function creates a physical volume of selected harddrive
+    :param drive: example "/dev/sda" with exit code
+    :return:
+    """
+    cmd = "pvcreate {}"
+    output = sp.getstatusoutput(cmd.format(drive))
+    return output
