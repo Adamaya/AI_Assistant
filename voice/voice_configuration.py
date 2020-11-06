8a7def2246048050eed741d21b00f14b7189e2b0
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-11-06 21:08:49 +0530

    linux voice configuration added

--- voice/voice_configuration.py
+++ voice/voice_configuration.py
@@ -1,10 +1,5 @@
 import os
 
-"""
-pyttsx3 is pytqhon library which is used to access the windows default voices 
-"""
-
-
 def speak(speach):
     """
     this function returns the audio output of AI
