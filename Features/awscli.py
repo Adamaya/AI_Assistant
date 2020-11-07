3aaf4f4f9050b7bf3dfbbf566ba311e286f56621
Archishman Ghosh <archis60.ag@gmail.com>
2020-11-07 12:26:20 +0530

    awscli update-1

--- Features/awscli.py
+++ Features/awscli.py
@@ -6,23 +6,30 @@ def aws_authentication(access_key, secret_key, region):
     secret_key = ""
     region = "ap-south-1"
 
-    os.system("aws configure set aws_access_key_id " + access_key)
-    os.system("aws configure set aws_secret_access_key " + secret_key)
-    os.system("aws configure set default.region " + region)
-    return None
+    out1 = os.system("aws configure set aws_access_key_id {}".format(access_key))
+    out2 = os.system("aws configure set aws_secret_access_key {}".format(secret_key))
+    out3 = os.system("aws configure set default.region {}".format(region))
+    return (out1,out2,out3)
+
+
+def create_key_pair(key_name):
+    output = os.system("aws ec2 create-key-pair --key-name {}".format(key_name))
+    return output
 
 
 def launch_ec2_instance():
     output = subprocess.getstatusoutput(
-        "aws ec2 run-instances --image-id ami-0e306788ff2473ccb --key-name myclikey --security-groups cliInstanceSecurityGroup --instance-type t2.micro")
-    return None
+        "aws ec2 run-instances --image-id ami-0e306788ff2473ccb --key-name myclikey --security-groups cliInstanceSecurityGroup --instance-type t2.micro"
+    )
+    return output
 
 
 def create_security_group():
     output = subprocess.getstatusoutput(
-        'aws ec2 create-security-group --group-name "cliInstanceSecurityGroup" --description "this is the security group created for the new instance" --vpc-id vpc-3fe8f557')
-    print(output)
-    return None
+        'aws ec2 create-security-group --group-name "cliInstanceSecurityGroup" --description "this is the security group created for the new instance" --vpc-id vpc-3fe8f557'
+    )
+    return output
 
 
-create_security_group()
+def create_volume():
+    output = subprocess.getstatusoutput()
